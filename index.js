4795c1d926db30221b5607ae74c18abfa6e62798
Elijah Matar <m.matar515@gmail.com>
2024-02-19 15:12:34 -0600

    Added more questions and edit grammar errors missed

--- index.js
+++ index.js
@@ -7,90 +7,56 @@ const questions = [
     {
         type: 'input',
         name: 'name',
-        message: "What is your GitHub username?",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username");
-                return false;
-            }
-        }
+        message: "What is your GitHub username?"
     },
     {
         type: 'input',
         name: 'email',
-        message: "What is your email address?",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username");
-                return false;
-            }
-        }
+        message: "What is your email address?"
     },
     {
         type: 'input',
         name: 'title',
-        message: "What is your project's name?",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username");
-                return false;
-            }
-        }
+        message: "What is the title of your project?"
     },
     {
         type: 'input',
         name: 'description',
-        message: "Please write a short desciption of your project:",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username");
-                return false;
-            }
-        }
+        message: "Please write a short desciption of your project:"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "What type of license should your project have?",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
     },
     {
         type: 'input',
-        name: 'name',
-        message: "What is your GitHub username?",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username");
-                return false;
-            }
-        }
+        name: 'dependencies',
+        default: 'npm i',
+        message: "What command should be run to install dependencies?"
     },
     {
         type: 'input',
-        name: 'name',
-        message: "What is your GitHub username?",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username");
-                return false;
-            }
-        }
+        name: 'test',
+        default: 'npm test',
+        message: "What command should be run to test application?"
     },
-
-
-    ,
-    "Please write a short desciption of your project:",
-    "What knd of license should your project have?",
-    "What command should be run to install dependencies?",
-    "What does the user need to know about using the repo?",
-    "What does the user need to know contributing to the repo?",
-    "Generating README..."
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "What does the user need to know about contributing to the repo?",
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "What are the test instructions?",
+    }
 ];
 
 // TODO: Create a function to write README file
