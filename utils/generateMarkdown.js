454c2f93005c958f41e472d6383890b23dbe01aa
Elijah Matar <m.matar515@gmail.com>
2024-02-19 19:52:53 -0600

    Added license badging

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,15 @@
-const fs = require('fs')
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  if (license === 'MIT'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (license === 'APACHE 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else if (license === 'GPL 3.0') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  } else if (license === 'BSD 3') {
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  }
 }
 
 // TODO: Create a function that returns the license link
@@ -16,36 +22,39 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # <Your-Project-Title>
-
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+  
   ## Description
-  
-  ## Table of Contents (Optional)
-  
+  ${data.description}
+
+  ## Table of Content
+  - [Description](#description)
   - [Installation](#installation)
   - [Usage](#usage)
   - [Credits](#credits)
   - [License](#license)
-  
+  - [Tests](#tests)
+  - [Contact](#github, #email)
+
   ## Installation
-  
+  ${data.installation}
+
   ## Usage
-  
-  ## Credits
-  
-  ## License
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-  
-  ## Features
-  
-  ## How to Contribute
-  
-  ## Tests
-  `;
+  ${data.usage}
+
+  ## Credits 
+  ${data.credits}
+
+  ## License 
+  ${data.license}
+
+  ## Tests 
+  ${data.test}
+
+  ## Contact
+  ${data.contact}
+
+  `
 }
 
 module.exports = generateMarkdown;
